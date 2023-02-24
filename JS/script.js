b1025086cf14b3d05518cd37c6b052e7b039a777
FNCV Florencia Celis <nataliac1914@gmail.com>
2023-02-24 11:17:01 -0300

    Primera escritura en el repositorio

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/*alert("Hola Bienvenido"); pasa a ser la prioridad
+confirm("Ingresar"); Con el botón, abre la posibilidad de cancelar*/
+
+function mensaje_alerta(){
+	alert("Hola Bienvenido"); /*pasa a ser la prioridad - */
+confirm("Ingresar"); /*Con el botón, abre la posibilidad de cancelar*/
+}
