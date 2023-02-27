df6b826ef0d087c2ce997ebffa81f2d5161f45cb
FNCV Florencia Celis <nataliac1914@gmail.com>
2023-02-27 12:10:34 -0300

    Modificacion icono y otros detalles

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,4 @@
-/*alert("Hola Bienvenido"); pasa a ser la prioridad
-confirm("Ingresar"); Con el botón, abre la posibilidad de cancelar*/
 
-function mensaje_alerta(){
-	alert("Hola Bienvenido"); /*pasa a ser la prioridad - */
-confirm("Ingresar"); /*Con el botón, abre la posibilidad de cancelar*/
+function mensaje_entrada(){
+	confirm("¡Sean bienvenidos!");
 }
